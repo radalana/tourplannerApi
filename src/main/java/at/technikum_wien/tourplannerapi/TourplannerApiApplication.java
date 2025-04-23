2b1aec870ff0dbc68cbe34fe36121076e14a53c9
radalana <radalana415@gmail.com>
2025-04-23 16:06:03 +0200

    Initial commit

---
+++ src/main/java/at/technikum_wien/tourplannerapi/TourplannerApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.tourplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourplannerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum_wien/tourplannerapi/TourplannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.tourplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourplannerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
