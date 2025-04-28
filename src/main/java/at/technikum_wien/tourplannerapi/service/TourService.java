c139a57fdcb2c4cf1b8f7dff5557ae9938c9f417
radalana <radalana415@gmail.com>
2025-04-28 16:20:37 +0200

    git commit -m 'TourService.java change path to backend api'

--- src/main/java/at/technikum_wien/tourplannerapi/service/TourService.java
+++ src/main/java/at/technikum_wien/tourplannerapi/service/TourService.java
@@ -1,6 +1,5 @@
 package at.technikum_wien.tourplannerapi.service;
 
-import at.technikum_wien.tourplannerapi.TourData;
 import at.technikum_wien.tourplannerapi.model.Tour;
 import at.technikum_wien.tourplannerapi.repository.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ public class TourService {
     private TourRepository repository;
 
     public Iterable<Tour> getAllTours() {
-        return TourData.getMockTours();
+        return repository.findAll();
     }
 
     public Tour getTourById(Long id) {
