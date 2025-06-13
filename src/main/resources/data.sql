7ca4217add61ec6e277972428b0dc70ec59e42c0
radalana <radalana415@gmail.com>
2025-06-13 18:43:23 +0200

    refactor: changed configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tours (
+    tour_name, description, from_location, to_location, distance, estimated_time, transport_type
+) VALUES
+      ('Wien Stadttour', 'Rundfahrt durch Wien', 'Stephansplatz', 'Schönbrunn', 5.2, 1.5, 'WALK'),
+      ('Alpenfahrt', 'Fahrt durch die Alpen', 'Innsbruck', 'Salzburg', 350.0, 4.0, 'CAR'),
+      ('Donau Radweg', 'Radtour entlang der Donau', 'Linz', 'Wien', 190.0, 6.0, 'BICYCLE');
