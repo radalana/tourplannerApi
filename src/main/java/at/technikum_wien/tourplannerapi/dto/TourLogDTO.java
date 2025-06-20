b5aa101caa4fa73321b058a05a98815bb11ec431
radalana <radalana415@gmail.com>
2025-06-20 18:15:03 +0200

    refactor TourLogMapper.java to date - string mapping

---
+++ src/main/java/at/technikum_wien/tourplannerapi/dto/TourLogDTO.java
@@ -0,0 +1,18 @@
+package at.technikum_wien.tourplannerapi.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class TourLogDTO {
+    private Long id;
+    private String dateTime;
+    private String comment;
+    private int difficulty;
+    private double totalDistance;
+    private double totalDuration;
+    private int rating;
+}
---
+++ src/main/java/at/technikum_wien/tourplannerapi/mapper/TourLogMapper.java
@@ -0,0 +1,22 @@
+package at.technikum_wien.tourplannerapi.mapper;
+
+import at.technikum_wien.tourplannerapi.dto.TourDTO;
+import at.technikum_wien.tourplannerapi.dto.TourLogDTO;
+import at.technikum_wien.tourplannerapi.dto.TourUpdateDTO;
+import at.technikum_wien.tourplannerapi.model.Tour;
+import at.technikum_wien.tourplannerapi.model.TourLog;
+import org.mapstruct.*;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TourLogMapper {
+    //public abstract void update(TourUpdateDTO dto, @MappingTarget Tour model);
+    @Mapping(source="date", target="dateTime", dateFormat = "dd-MM-yyyy")
+    public abstract TourLogDTO map(TourLog model);
+
+    @Mapping(source="dateTime", target="date", dateFormat = "dd-MM-yyyy")
+    public abstract TourLog map(TourLogDTO data);
+}
